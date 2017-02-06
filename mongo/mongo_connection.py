61dd0c4b18d82150650f3a75f78e41e6863a5907
raygao <ray@pilot.ai>
2017-02-06 08:38:00 -0800

    add frame loader

--- mongo/mongo_connection.py
+++ mongo/mongo_connection.py
@@ -6,12 +6,21 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import re
 import pymongo
+from numpy import uint32
+import warnings
+
+warnings.filterwarnings('ignore')
 
 class PilotMongo:
     def __init__(self,addr="mongodb://localhost:27017"):
         self.client = MongoClient(addr)
         print "init mongoclient, found these dbs: " + str(self.client.database_names())
         self.db = self.client.detection
+
+    def get_framepath_objects(self,frame_id):
+        query = {"_id":ObjectId(frame_id)}
+        cursor = self.db.frames.find(query)
+        return FrameLoader.frame_path(frame_id),[x['objects'] for x in cursor][0]
         
     def get_all_sets_ids(self):
         cursor = self.db.sets.find()
@@ -71,3 +80,51 @@ class PilotMongo:
         update = {'$set': {'tags': new_tags}}
         return mon.db.sets.update_one(query,update)
 
+class FrameLoader:
+
+    @staticmethod
+    def hash_(key):
+        key += ~(key << uint32(15));
+        key ^=  (key >> uint32(10));
+        key +=  (key << uint32(3));
+        key ^=  (key >> uint32(6));
+        key += ~(key << uint32(11));
+        key ^=  (key >> uint32(16));
+        return key;
+
+    @staticmethod
+    def hash3(a,b,c):
+        a-=b;a-=c;a^=(c>>uint32(13));
+        b-=c;b-=a;b^=(a<<uint32(8));
+        c-=a;c-=b;c^=(b>>uint32(13));
+        a-=b;a-=c;a^=(c>>uint32(12));
+        b-=c;b-=a;b^=(a<<uint32(16));
+        c-=a;c-=b;c^=(b>>uint32(5));
+        a-=b;a-=c;a^=(c>>uint32(3));
+        b-=c;b-=a;b^=(a<<uint32(10));
+        c-=a;c-=b;c^=(b>>uint32(15));
+        return c;
+
+    @staticmethod
+    def hash_str(s):
+      current_hash = FrameLoader.hash_(uint32(ord(s[0])));
+      for i in range(1,len(s)):
+        current_hash = FrameLoader.hash3(current_hash, uint32(ord(s[i])), uint32(15760506));
+
+      return current_hash;
+
+    @staticmethod
+    def objectid_to_dir(_id):
+        mask = uint32(255)
+        hashcode = FrameLoader.hash_str(_id)
+        first_dir = hashcode & mask
+        second_dir = (hashcode >> 8) & mask
+        return os.path.join("%03d"%int(first_dir), "%03d"%int(second_dir))
+
+    @staticmethod
+    def video_path(set_id,data_basedir='/local_data/DataServer'):
+        return os.path.join(data_basedir,"Videos", FrameLoader.objectid_to_dir(set_id), set_id)
+
+    @staticmethod
+    def frame_path(frame_id,data_basedir='/local_data/DataServer'):
+        return os.path.join(data_basedir,"Frames", FrameLoader.objectid_to_dir(frame_id), frame_id)
