3709ca5ead41a9e08f8cbabf4050de4c32b6ccb7
ray <raym.gao@gmail.com>
2017-12-18 22:53:22 -0800

    cleanup

--- pytlib/configuration/config_example.py
+++ pytlib/configuration/config_example.py
@@ -6,15 +6,24 @@ import torch.optim as optim
 import torch.nn as nn
 from networks.autoencoder import AutoEncoder
 
-# define these things here
 def get_sampler():
     source = KITTISource('/home/ray/Data/KITTI/training',max_frames=10)
     sampler_params = {'crop_size':[255,255],'obj_types':['Car']}
     return AutoEncoderSampler(source,sampler_params)
 
+# loader + args for the loader
 loader = (MultiSampler,dict(loader=get_sampler,loader_args=dict(),num_procs=2))
+
+# model + args for the model
 model = (AutoEncoder,dict())
+
+# optimizer + args
 optimizer = (optim.Adam,dict(lr=1e-3))
+
+# the loss function
 loss = nn.BCELoss()
+
+# create the config object, this object is then used by the trainer to access all the above
+# components for training a model
 train_config = TrainConfiguration(loader,optimizer,model,loss,False)
 
