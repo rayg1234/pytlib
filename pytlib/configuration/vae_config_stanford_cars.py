14a7b1c6cb34f7a246baa96c7ab4bb5d7b734dd3
ray gao <raym.gao@gmail.com>
2017-12-28 09:56:05 -0800

    cleanup vae config

--- pytlib/configuration/vae_config_stanford_cars.py
+++ pytlib/configuration/vae_config_stanford_cars.py
@@ -3,15 +3,13 @@ from data_loading.sources.stanford_cars_source import StanfordCarsSource
 from data_loading.loaders.multi_loader import MultiLoader
 from data_loading.loaders.autoencoder_loader import AutoEncoderLoader
 import torch.optim as optim
-import torch.nn as nn
 from networks.vae import VAE
 from loss_functions.vae_loss import vae_loss
-import random
 
 def get_loader():
-	source = StanfordCarsSource(cars_dir='/home/ray/Data/StanfordCars/cars_train',
-								labels_mat='/home/ray/Data/StanfordCars/devkit/cars_train_annos.mat')
-	return AutoEncoderLoader(source,{'crop_size':[100,100],'obj_types':'car'})
+    source = StanfordCarsSource(cars_dir='/home/ray/Data/StanfordCars/cars_train',
+                                labels_mat='/home/ray/Data/StanfordCars/devkit/cars_train_annos.mat')
+    return AutoEncoderLoader(source,{'crop_size':[100,100],'obj_types':'car'})
 loader = (MultiLoader,dict(loader=get_loader,loader_args=dict(),num_procs=10))
 model = (VAE,dict(encoding_size=128,training=True))
 optimizer = (optim.Adam,dict(lr=1e-3))
