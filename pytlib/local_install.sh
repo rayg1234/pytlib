d60a09f4d4ce788e5d916fefe169b4365825cfa8
ray <raym.gao@gmail.com>
2018-05-16 15:09:24 -0700

    add explict cython to install

--- pytlib/local_install.sh
+++ pytlib/local_install.sh
@@ -7,4 +7,5 @@ virtualenv pytenv
 pwd > pytenv/lib/python2.7/site-packages/pyenv.pth
 source pytenv/bin/activate
 #pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp27-cp27mu-manylinux1_x86_64.whl
+pip install Cython
 pip install -r requirements.txt
