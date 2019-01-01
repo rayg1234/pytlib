8cce8ac0df1d96d5a75a8d3739b352d79398c164
ray <raym.gao@gmail.com>
2018-12-31 16:14:47 -0800

    forgot to add files

---
+++ pytlib/utils/batch_box_utils.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+def rescale_boxes(boxes, scale, offset=[0,0]):
+    assert boxes.shape[1]==4, 'dim[1] of boxes must the box dimensions, instead got {}'.format(boxes.shape)
+    height,width = scale
+    new_boxes = boxes.clone()
+    new_boxes[:,[1,3]] = boxes[:,[1,3]]*height + offset[0]
+    new_boxes[:,[0,2]] = boxes[:,[0,2]]*width + offset[1]
+    return new_boxes
+
+def batch_box_intersection(t1,t2):
+    assert len(t1.shape)==2 and len(t2.shape)==2 and \
+        t1.shape[1]==4 and t2.shape[1]==4, 'boxes must be Nx4 tensors'
+    x_min1,y_min1,x_max1,y_max1 = torch.chunk(t1,4,dim=1)
+    x_min2,y_min2,x_max2,y_max2 = torch.chunk(t2,4,dim=1)
+    y_mins = torch.max(y_min1,y_min2.transpose(0,1))
+    y_maxs = torch.min(y_max1,y_max2.transpose(0,1))
+    intersect_heights = torch.max(torch.zeros_like(y_maxs), y_maxs - y_mins)
+    x_mins = torch.max(x_min1,x_min2.transpose(0,1))
+    x_maxs = torch.min(x_max1,x_max2.transpose(0,1))
+    intersect_widths = torch.max(torch.zeros_like(x_maxs), x_maxs - x_mins)
+    result = intersect_heights * intersect_widths 
+    return result
+
+def batch_box_area(boxes):
+    assert len(boxes.shape)==2 and boxes.shape[1]==4, 'boxes must be Nx4 tensors'
+    x_min,y_min,x_max,y_max = torch.chunk(boxes,4,dim=1)
+    area = ((x_max - x_min)*(y_max - y_min))
+    return area
+
+def batch_box_IOU(t1,t2):
+    intersections = batch_box_intersection(t1,t2)
+    width,height = intersections.shape
+    areas1 = batch_box_area(t1)
+    areas2 = batch_box_area(t2)
+    areas1_expanded = areas1.expand(-1,height)
+    areas2_expanded = areas2.transpose(0,1).expand(width,-1)
+    unions = (areas1_expanded + areas2_expanded - intersections)
+    iou = torch.where(
+        torch.eq(intersections, 0.0),
+        torch.zeros_like(intersections), torch.div(intersections, unions))        
+    return iou
+
+def euc_distance_cost(boxes1,boxes2):
+    assert len(boxes1.shape)==2 and boxes1.shape[1]==4, 'boxes must be Nx4 tensors'
+    assert len(boxes2.shape)==2 and boxes2.shape[1]==4, 'boxes must be Nx4 tensors'
+    # center squared cost: ((xmax1+xmin1)/2 - (xmax2+xmin2)/2))^2
+    x_min1,y_min1,x_max1,y_max1 = torch.chunk(boxes1,4,dim=1)
+    x_min2,y_min2,x_max2,y_max2 = torch.chunk(boxes2,4,dim=1)
+    cx1 = (x_max1+x_min1)/2
+    cx2 = (x_max2+x_min2)/2
+    cy1 = (y_max1+y_min1)/2
+    cy2 = (y_max2+y_min2)/2
+    xx1,xx2 = torch.meshgrid(cx1.squeeze(),cx2.squeeze())
+    yy1,yy2 = torch.meshgrid(cy1.squeeze(),cy2.squeeze())
+    distx = (xx2-xx1)*(xx2-xx1)
+    disty = (yy2-yy1)*(yy2-yy1)
+    return distx+disty
+
+def generate_region_meshgrid(num_regions,region_size,region_offsets):
+    hh,ww = torch.meshgrid(torch.arange(0,num_regions[0]),torch.arange(0,num_regions[1]))
+    hh = (hh*region_size[0]+region_offsets[0]).cuda().float()
+    ww = (ww*region_size[1]+region_offsets[1]).cuda().float()
+    return hh,ww
---
+++ pytlib/utils/batch_box_utils_test.py
@@ -0,0 +1,98 @@
+import unittest
+import torch
+from utils.batch_box_utils import batch_box_intersection, batch_box_area, batch_box_IOU, rescale_boxes, euc_distance_cost
+from utils.test_utils import near_tensor_equality
+
+class TestBatchBoxUtils(unittest.TestCase):
+
+    def test_rescale_boxes_no_offset(self):
+        boxes = torch.Tensor([[0,0,1,1],
+                              [2,2,4,4]])
+        scale = [1,2]
+        scaled_boxes = rescale_boxes(boxes, scale)
+        expected_boxes = torch.Tensor([[0.,0.,2.,1.],
+                                       [4.,2.,8.,4.]])
+        # print expected_boxes
+        self.assertTrue(near_tensor_equality(scaled_boxes,expected_boxes))
+
+    def test_rescale_boxes_with_offset(self):
+        boxes = torch.Tensor([[0,0,1,1],
+                              [2,2,4,4]])
+        scale = [1,2]
+        offset = [40,30]
+        scaled_boxes = rescale_boxes(boxes, scale, offset)
+        expected_boxes = torch.Tensor([[30.,40.,32.,41.],
+                                       [34.,42.,38.,44.]])
+        # print scaled_boxes
+        self.assertTrue(near_tensor_equality(scaled_boxes,expected_boxes))
+
+    def test_batch_box_IOU_edge_cases(self):
+        t1 = torch.Tensor([[0,0,0,0]])
+        t2 = torch.Tensor([[0,0,0,0]])
+        ious = batch_box_IOU(t1,t2)
+        expected_matrix = torch.Tensor([[0.]])
+        self.assertTrue(near_tensor_equality(ious,expected_matrix))
+
+    def test_euc_distance_cost(self):
+        t1 = torch.Tensor([[0,0,1,1],
+                           [2,2,8,8],
+                           [-1,-1,0,0]])
+        t2 = torch.Tensor([[0,0,1,1],
+                           [3,3,9,9]])
+        distmat = euc_distance_cost(t1,t2)
+        expected_matrix = torch.Tensor([[ 0.0000, 60.5000],
+                                        [40.5000,  2.0000],
+                                        [ 2.0000, 84.5000]])
+        self.assertTrue(near_tensor_equality(distmat,expected_matrix))
+
+    def test_batch_box_IOU(self):
+        t1 = torch.Tensor([[0,0,1,1],
+                           [2,2,8,8],
+                           [-1,-1,0,0]])
+        t2 = torch.Tensor([[0,0,1,1],
+                           [3,3,9,9]])
+        ious = batch_box_IOU(t1,t2)
+        expected_matrix = torch.Tensor([[1.0000, 0.0000],
+                                        [0.0000, 0.5319],
+                                        [0.0000, 0.0000]])
+        self.assertTrue(near_tensor_equality(ious,expected_matrix))
+
+    def test_batch_box_intersection_edge_cases(self):
+        # no intersection
+        t1 = torch.Tensor([[0,0,1,1]])
+        t2 = torch.Tensor([[3,4,9,10],
+                           [3,3,9,9]])
+        intersection_matrix = batch_box_intersection(t1,t2)
+        expected_matrix = torch.Tensor([[ 0.],
+                                        [ 0.]])
+        self.assertTrue(torch.all(torch.eq(intersection_matrix,expected_matrix)))
+
+        # negative intersection
+        t1 = torch.Tensor([[0,0,-1,-1]])
+        t2 = torch.Tensor([[-3,-4,9,10],
+                           [-3,-3,9,9]])
+        intersection_matrix = batch_box_intersection(t1,t2)
+        expected_matrix = torch.Tensor([[ 0.],
+                                        [ 0.]])
+        self.assertTrue(torch.all(torch.eq(intersection_matrix,expected_matrix)))
+
+    def test_batch_box_intersection(self):
+        t1 = torch.Tensor([[0,0,1,1],
+                           [2,2,8,8]])
+        t2 = torch.Tensor([[0,0,1,1],
+                           [3,3,9,9]])
+        intersection_matrix = batch_box_intersection(t1,t2)
+        expected_matrix = torch.Tensor([[ 1.,  0.],
+                                        [ 0., 25.]])
+        self.assertTrue(torch.all(torch.eq(intersection_matrix,expected_matrix)))
+
+    def test_batch_box_area(self):
+        boxes = torch.Tensor([[0,0,1,1],
+                              [2,2,8,8],
+                              [0,0,0,0]])
+        areas = batch_box_area(boxes)
+        expected_areas = torch.tensor([[1.,36.,0.]]).transpose(0,1)
+        self.assertTrue(near_tensor_equality(areas,expected_areas))
+
+if __name__ == '__main__':
+    unittest.main()
