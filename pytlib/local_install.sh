04786f2a036ff107fb034dd1e6022017739c6b45
ray gao <raym.gao@gmail.com>
2019-11-17 10:02:09 -0800

    update

--- pytlib/local_install.sh
+++ pytlib/local_install.sh
@@ -3,10 +3,7 @@
 sudo apt install python3-tk
 sudo apt install graphviz
 pip3 install pipenv
-# fix this to use the actual python version
 pypath=$(pipenv --venv)
 pwd > $pypath/lib/python3.5/site-packages/pyenv.pth
 pipenv shell
 pipenv install
-#pip3 install Cython
-#pip3 install -r requirements.txt
