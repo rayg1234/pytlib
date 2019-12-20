e41bee35b2358a4aff97c742909bf77d73c4e4e3
ray <raym.gao@gmail.com>
2019-12-19 21:47:47 -0800

    update install

--- pytlib/local_install.sh
+++ pytlib/local_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#sudo apt install python3-pip
+sudo apt install python3-pip
 sudo apt install python3-tk
 sudo apt install graphviz
 pip3 install pipenv
 pypath=$(pipenv --venv)
 pwd > $pypath/lib/python3.5/site-packages/pyenv.pth
-pipenv shell
 pipenv install
+pipenv shell
